0cb8b89c0411f9e4c15386cddc4b757fb367d34c
Jihye Kim / Gigi <113292958+gigi3074@users.noreply.github.com>
2023-05-21 21:16:20 +0900

    파일 전체 구현

---
+++ Chapter.12/gigi3074/예제문제/gigi3074/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+            중복으로 풀어야되는 문제도 있기에
+            네이밍 누구나 알 수 있게 구현 및
+            함수화 처리해서 중복 없애는 방식으로 구현해보기
+         */
+        
+        System.out.println("Hello world!");
+    }
+}
